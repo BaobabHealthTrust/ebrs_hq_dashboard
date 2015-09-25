f6a34568ae49ebf4c72727ba3d72801cbda76b24
Blessings <blessings.mhango@gmail.com>
2015-09-25 15:15:32 +0200

    passing date parameters

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   def index
      start_date = 3.months.ago.to_date.strftime("%Y-%m-%d")
      end_date =  Date.today.strftime("%Y-%m-%d")
-     @dc = Dc.by_date_created_range.startkey(end_date).endkey(start_date).each
+     @dc = Dc.by_date_created_range.startkey(start_date).endkey(endkey).each
      @hq = Hq.all.each
   end
 end
--- app/models/dc.rb
+++ app/models/dc.rb
@@ -19,18 +19,22 @@ class Dc < CouchRest::Model::Base
     view :by_date_created_range,
          :map => "function(doc) {
                   if (doc['type'] == 'Dc' && doc['date_created'] != null) {
-                      if (doc['date_created'].trim().match(/^(\\d{2}|\\d{1})\\/[A-Z][a-z]{2}\\/\\d{4}/) ||
+                    if (doc['date_created'].trim().match(/^(\\d{2}|\\d{1})\\/[A-Z][a-z]{2}\\/\\d{4}/) ||
                         doc['date_created'].trim().match(/^(\\d{2}|\\d{1})\s[A-Z][a-z]{2},\s\\d{4}/)) {
                             var tokens = doc['date_created'].replace(/,/, '').replace(/\\//g, ' ').split(' ');
                             var months = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',
                               'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'};
                             var date = tokens[2] + '-' + months[tokens[1]] + '-' + tokens[0];
                             emit((new Date(date)), 1);
-                      } else {
-                        emit((new Date(doc['date_created'])), 1);
-                      }
+                    } else if(doc['date_created'].trim().length > 10){
+                        var date = doc['date_created'].trim().substring(0, 10);
+                        emit((new Date(date)), 1);
+                    } else {
+                      emit((new Date(doc['date_created'])), 1);
+                    }
                   }
                 }"
+
 
   end 
   
