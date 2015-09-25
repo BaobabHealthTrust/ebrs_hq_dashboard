7220189eb7ab6fc3832ff38eae20c1f6c61319e7
Blessings <blessings.mhango@gmail.com>
2015-09-25 14:51:30 +0200

    added date created

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
-     @dc = Dc.all.each
+     start_date = 3.months.ago.to_date.strftime("%Y-%m-%d")
+     end_date =  Date.today.strftime("%Y-%m-%d")
+     @dc = Dc.by_date_created_range.startkey(end_date).endkey(start_date).each
      @hq = Hq.all.each
   end
 end
