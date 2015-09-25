22d05a0d7a630413466428f2d98c80d89bcd2211
Blessings <blessings.mhango@gmail.com>
2015-09-25 14:08:33 +0200

    removed unnecessary config files

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,1 @@
 CONF = YAML.load_file(Rails.root.join('config', 'couchdb.yml'))[Rails.env]
-DC = YAML.load_file(Rails.root.join('config', 'dc_config.yml'))
-DCDESKTOP = YAML.load_file(Rails.root.join('config', 'dc_desktop_config.yml'))
-HQ = YAML.load_file(Rails.root.join('config', 'hq_config.yml'))
