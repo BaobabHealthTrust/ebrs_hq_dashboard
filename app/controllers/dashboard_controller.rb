0c9ad5e048c37259ab1fca9c93211ebad076ed93
Blessings <blessings.mhango@gmail.com>
2015-11-10 09:05:19 +0200

    added get data method

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,23 @@ class DashboardController < ApplicationController
      @dc = Dc.by_date_created_range.startkey(start_date).endkey(end_date).each
      @hq = Hq.all.each
   end
+  
+  def get_data
+  
+  file_name = Rails.root.join('docs', 'statistics.txt')
+  file = YAML.load_file(file_name)
+  scores = {}
 
+  (file || []).each do |district, statistics|
+    scores[district]= {}
+    (statistics || []).each do |type, statistic|
+      scores[district][type] = statistic
+    end
+  end
+
+  render :text => scores.to_json
+     
+  end
   def map_dashboard
       @url = "http://#{MAP_CONFIG['user']}:#{MAP_CONFIG['password']}@#{MAP_CONFIG['host']}#{MAP_CONFIG['url']}scaling_factor=#{MAP_CONFIG['scaling_factor']}&scroll=#{MAP_CONFIG['scroll']}"
   end
